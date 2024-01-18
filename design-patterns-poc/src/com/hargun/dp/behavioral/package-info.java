54515bbd55843165d46f3d762b2d345c6ade8ec7
Hargun Suri <hargun.suri@cerillion.com>
2024-01-18 12:53:11 +0530

    011820241250 - [POC][STRUCTURE]

---
+++ design-patterns-poc/src/com/hargun/dp/behavioral/package-info.java
@@ -0,0 +1,36 @@
+package com.hargun.dp.behavioral;
+
+/**
+ * 
+ * Behavioral design patterns focus on how objects collaborate and communicate
+ * to accomplish specific tasks.
+ * 
+ * They help in defining and organizing responsibilities, and communication
+ * strategies between objects, promoting flexibility and reusability in software
+ * design.
+ * 
+ * 
+ * List of Behavioral design patterns -
+ * 
+ * Chain of Responsibility Pattern
+ * 
+ * Command Pattern
+ * 
+ * Interpreter Pattern
+ * 
+ * Iterator Pattern
+ * 
+ * Mediator Pattern
+ * 
+ * Memento Pattern
+ * 
+ * Observer Pattern
+ * 
+ * State Pattern
+ * 
+ * Strategy Pattern
+ * 
+ * Template Method Pattern
+ * 
+ * Visitor Pattern
+ */
---
+++ design-patterns-poc/src/com/hargun/dp/creational/package-info.java
@@ -0,0 +1,1 @@
+package com.hargun.dp.creational;
